3352e3458abe98b70c4536ed9c4ca1b15e5181be
Maverick Chan <guanhao3797@gmail.com>
2014-08-16 22:39:29 +0800

    Comments added

--- google.com.js
+++ google.com.js
@@ -4,8 +4,10 @@
 
 var adsDiv = $('div#taw');
 
+// Removes ads on page load
 adsDiv.remove();
 
+// Removes ads on Search button click
 $('button#gbqfb').click(function() {
   adsDiv.remove();
 });
